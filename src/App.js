4fc103f6da9371521d1b893db0f03bb4da00db26
rathkemauricio@gmail.com <rathkemauricio@gmail.com>
2024-08-26 22:00:17 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import react, {useEffect, useState}from 'react';
+///https://sujeitoprogramador.com/rn-api/?api=posts
+import './style.css';
+
+
+function App() {
+
+  const[nutri, setNutri] = useState([]);
+
+  useEffect(()=>{
+
+    function loadApi(){
+
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+
+      fetch( url)
+      .then((r)=> r.json() )
+      .then((json)=>{
+        console.log(json);
+        setNutri(json);
+
+      })
+
+    }
+
+    loadApi();
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item)=>{
+        return(
+        <article key={item.id} className="post">
+          <strong className="titulo">{item.titulo}</strong>
+
+          <img src={item.capa} alt={item.titulo} className="capa"/>
+        <p className="subtitulo">
+          {item.subtitulo}
+        </p>
+        
+        <a className="botao">Acessar</a>
+        </article>
+      )
+    })}
+    
+    </div>
+  );
+}
+
+export default App;
